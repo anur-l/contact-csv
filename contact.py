8b6c90f21c55c09a238699389ddb16910ee52568
aj <amjamal4050@gmail>
2026-05-24 09:06:13 +0300

    Adding main funcation

--- contact.py
+++ contact.py
@@ -4,13 +4,21 @@ list_person: list[dict[str, str]] = [
     {"name": "Mariam", "phone": "93284", "email": "mar0@mail.com"},
 ]
 
-def view_contact(list_person:list[dict[str,str]])-> None:
+def view_contact(contact :list[dict[str,str]])-> None:
     print('=== === === === === ===')
     no: int = 1
-    for i in list_person:
+    for i in contact:
         print(f'|{no}| Name: {i['name']}')
         no += 1
     print('=== === === === === ===')
 
-view_contact(list_person)
-    
+def main () -> None:
+    list_person: list[dict[str, str]] = [
+        {"name": "Ahmed", "phone": "89892", "email": "test@info.com"},
+        {"name": "Faisal", "phone": "67676", "email": "fa@11dev.com"},
+        {"name": "Mariam", "phone": "93284", "email": "mar0@mail.com"},
+    ]
+    view_contact(list_person)
+
+if __name__ == "__main__":
+    main()
