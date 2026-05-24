010d594c0a6e7db3b827d7690180690970ab2fbc
aj <amjamal4050@gmail>
2026-05-24 09:21:49 +0300

    beta: Adding get_name fucntion and aslo menu for start up

--- contact.py
+++ contact.py
@@ -1,8 +1,9 @@
-list_person: list[dict[str, str]] = [
-    {"name": "Ahmed", "phone": "89892", "email": "test@info.com"},
-    {"name": "Faisal", "phone": "67676", "email": "fa@11dev.com"},
-    {"name": "Mariam", "phone": "93284", "email": "mar0@mail.com"},
-]
+
+import sys
+
+
+MAX_ATTEMPT:int = 3
+
 
 def view_contact(contact :list[dict[str,str]])-> None:
     print('=== === === === === ===')
@@ -11,6 +12,23 @@ def view_contact(contact :list[dict[str,str]])-> None:
         print(f'|{no}| Name: {i['name']}')
         no += 1
     print('=== === === === === ===')
+
+def get_name() -> str:
+    print('> ',end="",flush=True)
+    name:str = sys.stdin.readline()
+    return name 
+        
+
+
+def menu() -> None:
+    state:bool = True 
+    while state:
+        print("=== Contact info ===")
+        print("1.) View")
+        print("2.) Add")
+
+
+
 
 def main () -> None:
     list_person: list[dict[str, str]] = [
